e1965c1c70bc626c1bbc3504430533eb4ef4e4c5
NoeG47 <hellobrawlstars2.0@gmail.com>
2024-01-13 16:26:13 +0100

    Commit Volumen Cubo N

--- src/Caso3/Ej_Caso3.java
+++ src/Caso3/Ej_Caso3.java
@@ -42,19 +42,28 @@ public class Ej_Caso3 {
 		alturaCilindro = in.nextDouble();
 		System.out.println("El área de la cilindro es: " + calcularAreaCilindro(radioCilindro, alturaCilindro));
 		System.out.println("\n");
-		
-		//Alumno2: 
-		//CASO RECTÁNGULO L 
+
+		// Alumno2:
+		// CASO RECTÁNGULO L
 		System.out.println("CASO RECTÁNGULO");
 		double longitud = 0;
 		double ancho = 0;
 		// Calcular el área de un rectángulo
 		System.out.print("Ingrese la longitud del rectángulo: ");
-        longitud = in.nextDouble();
-        System.out.print("Ingrese el ancho del rectángulo: ");
-        ancho = in.nextDouble();
-        System.out.println("El área de un rectángulo es: " + calcularAreaRectanguloL(longitud,ancho));
-        System.out.println("\n");
+		longitud = in.nextDouble();
+		System.out.print("Ingrese el ancho del rectángulo: ");
+		ancho = in.nextDouble();
+		System.out.println("El área de un rectángulo es: " + calcularAreaRectanguloL(longitud, ancho));
+		System.out.println("\n");
+
+		// Alumno1:
+		// CASO VOLUMEN CUBO N
+		System.out.println("CASO CUBO");
+		double ladoCubo = 0;
+		System.out.println("Introduce el lado del cubo: ");
+		ladoCubo = in.nextDouble();
+		System.out.println("El volumen del cubo es: " + calcularVolumenCuboN(ladoCubo));
+		System.out.println("\n");
 
 	}
 
@@ -72,6 +81,7 @@ public class Ej_Caso3 {
 	public static double calcularPerimetroCircunferencia(double radio) {
 		return 2 * 3.14 * radio;
 	}
+
 	// CASO CILINDRO N
 	public static double calcularAreaCilindro(double radioCilindro, double alturaCilindro) {
 		double pi = 3.14;
@@ -85,9 +95,14 @@ public class Ej_Caso3 {
 		return areaTotal;
 
 	}
-	//CASO RECTÁNGULO L
+
+	// CASO RECTÁNGULO L
 	public static double calcularAreaRectanguloL(double longitud, double ancho) {
 		return longitud * ancho;
 	}
 
+	// CASO VOLUMEN CUBO N
+	public static double calcularVolumenCuboN(double ladoCubo) {
+		return ladoCubo * ladoCubo * ladoCubo;
+	}
 }
